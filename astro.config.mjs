a8b466aefe6d20adf0d6583c5f9fe4f9fa380b73
纸橙 <3529875583@qq.com>
2026-04-30 22:34:13 +0800

    fix: 移除 sitemap 插件以修复构建错误

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://zhichengzc.github.io',
   base: '/ZhichengAnimationHome',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), tailwind()],
   markdown: {
     shikiConfig: {
       theme: 'github-light',
